f79fb5f59db39809754da0e829399656f778b152
Clara Ng <x3Clarawork@gmail.com>
2026-04-03 20:14:51 +0800

    Fix merge conflicts

--- src/main/java/seedu/address/logic/parser/BulkIndexParserUtil.java
+++ src/main/java/seedu/address/logic/parser/BulkIndexParserUtil.java
@@ -170,7 +170,7 @@ public final class BulkIndexParserUtil {
     private static void parseSingleToken(String token, Set<Integer> indexSet)
             throws ParseException {
 
-        // Validate format first (digits only)
+        // Validate format first (digit only)
         if (!token.matches("\\d+")) {
             throw new ParseException(MESSAGE_INVALID_TOKEN);
         }
